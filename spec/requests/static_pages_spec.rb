e234440f82ea37aceb7e5f1f402dbe5a37f6ad72
wcagle <wcagle@financialpayments.com>
2012-06-20 15:38:21 -0500

    test static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,9 +12,13 @@ describe "Static pages" do
   	it "should have the right title" do
   		visit '/static_pages/home'
   		page.should have_selector('title', 
-  		                   :text => "Ruby Sample App | Home")  		
+  		                   :text => "Ruby Sample App")  		
   	end
 
+  	it "should not have a custom page title" do
+  		visit '/static_pages/home'
+  		page.should_not have_selector('title', :text => '| Home')
+  	end
 
   end
 
